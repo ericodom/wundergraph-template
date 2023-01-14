cbf64af49df58ea7da52b382275fa18081c7653a
Eric Odom <eric@homecareintel.com>
2023-01-14 08:32:02 -1000

    add loading server component

---
+++ src/app/new/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading(): JSX.Element {
+	return (
+		<div className="flex items-center justify-center pt-16">
+			<p className="text-2xl font-bold text-white">
+				Loading (with loading.tsx!)...
+			</p>
+		</div>
+	);
+}
